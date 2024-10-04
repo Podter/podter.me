27ab2b47ee4209dbbfc06413403661968031f291
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-10-04 22:58:43 +0700

    added cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 // @ts-check
 
-import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+    imageService: "cloudflare",
+  }),
   integrations: [tailwind()],
 });
