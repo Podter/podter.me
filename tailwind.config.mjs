91cffeed4366eefaf7b51a72b1cf7c665eb6974f
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-10-04 23:37:22 +0700

    added geist

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
 
+import { fontFamily } from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["GeistSans", ...fontFamily.sans],
+        mono: ["GeistMono", ...fontFamily.mono],
+      },
+    },
   },
   plugins: [],
 };
