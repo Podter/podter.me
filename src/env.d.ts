605609dd69435bf995817041a247d304c5a7203b
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-10-06 17:02:50 +0700

    added basic auth

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,14 @@
 /* eslint-disable */
 /// <reference path="../.astro/types.d.ts" />
+interface Env {
+  AUTH_SECRET: string;
+  DISCORD_ID: string;
+  DISCORD_SECRET: string;
+  DISCORD_BOT_TOKEN: string;
+  GITHUB_ID: string;
+  GITHUB_SECRET: string;
+}
+
 type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
 
 declare namespace App {
