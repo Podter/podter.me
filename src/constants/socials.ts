78c069c4d401f946b66e94ff146957402f05e1ff
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-10-08 23:22:07 +0700

    added social links

--- src/constants/socials.ts
+++ src/constants/socials.ts
@@ -1,1 +1,16 @@
 export const DISCORD_ID = "331793642689789962";
+
+export const SOCIALS = {
+  github: {
+    href: "https://github.com/Podter",
+  },
+  discord: {
+    href: `https://discord.com/users/${DISCORD_ID}`,
+  },
+  x: {
+    href: "https://x.com/Real_Podter",
+  },
+  email: {
+    href: "mailto:hello@podter.me",
+  },
+} as const as Record<string, { href: string }>;
