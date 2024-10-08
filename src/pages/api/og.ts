2a6d4dfee7f6ea8996d63b5e18a516129dbffee6
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-10-08 14:32:12 +0700

    fix size limit

--- src/pages/api/og.ts
+++ src/pages/api/og.ts
@@ -1,7 +1,4 @@
 import type { APIRoute } from "astro";
-import { initWasm, Resvg } from "@resvg/resvg-wasm";
-// @ts-expect-error .wasm files are not typed
-import resvgWasm from "@resvg/resvg-wasm/index_bg.wasm";
 import satori from "satori";
 import { html } from "satori-html";
 
@@ -9,66 +6,40 @@ import { OG_HEIGHT, OG_WIDTH } from "~/constants/metadata";
 
 export const prerender = false;
 
-async function initResvg() {
-  try {
-    await initWasm(resvgWasm as WebAssembly.Module);
-  } catch (err) {
-    if (err instanceof Error && err.message.includes("Already initialized")) {
-      return;
-    }
-    throw err;
-  }
-}
+export const GET: APIRoute = async ({ url }) => {
+  const title = url.searchParams.get("title");
+  const description = url.searchParams.get("description");
 
-export const GET: APIRoute = async ({ request, url, locals }) => {
-  // @ts-expect-error - request types is wrong
-  let response: Response | undefined =
-    // @ts-expect-error - request types is wrong
-    await locals.runtime.caches.default.match(request);
+  const newsreaderFont = await fetch(
+    "https://cdn.jsdelivr.net/fontsource/fonts/newsreader@latest/latin-500-normal.ttf",
+  ).then((res) => res.arrayBuffer());
 
-  if (!response) {
-    const title = url.searchParams.get("title");
-    const description = url.searchParams.get("description");
+  const markup = html`<div
+    style="color: black; font-family: Newsreader; font-size: 72;"
+  >
+    ${title} | ${description}
+  </div>`;
 
-    const newsreaderFont = await fetch(
-      "https://cdn.jsdelivr.net/fontsource/fonts/newsreader@latest/latin-500-normal.ttf",
-    ).then((res) => res.arrayBuffer());
-
-    const markup = html`<div
-      style="color: black; font-family: Newsreader; font-size: 72;"
-    >
-      ${title} | ${description}
-    </div>`;
-
-    const svg = await satori(markup, {
-      width: OG_WIDTH,
-      height: OG_HEIGHT,
-      fonts: [
-        {
-          name: "Newsreader",
-          data: newsreaderFont,
-          style: "normal",
-          weight: 500,
-        },
-      ],
-    });
-
-    await initResvg();
-    const resvg = new Resvg(svg);
-    const pngData = resvg.render();
-    const pngBuffer = pngData.asPng();
-
-    response = new Response(pngBuffer, {
-      headers: {
-        "Content-Type": "image/png",
-        "Cache-Control": "public, immutable, no-transform, max-age=31536000",
+  const svg = await satori(markup, {
+    width: OG_WIDTH,
+    height: OG_HEIGHT,
+    fonts: [
+      {
+        name: "Newsreader",
+        data: newsreaderFont,
+        style: "normal",
+        weight: 500,
       },
-    });
-    locals.runtime.ctx.waitUntil(
-      // @ts-expect-error - request types is wrong
-      locals.runtime.caches.default.put(request, response.clone()),
-    );
-  }
+    ],
+  });
 
-  return response;
+  const png = await fetch("https://svg-to-png.podter.workers.dev", {
+    method: "POST",
+    body: svg,
+    headers: {
+      "Content-Type": "image/svg+xml",
+    },
+  });
+
+  return png;
 };
