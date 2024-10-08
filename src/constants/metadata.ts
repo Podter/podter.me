ab57f9dc468b1775e020038d61e5d2095d024270
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-10-08 14:26:08 +0700

    added working og

--- src/constants/metadata.ts
+++ src/constants/metadata.ts
@@ -5,5 +5,5 @@ export const DESCRIPTION = "A student and self-taught developer from Thailand.";
 export const OG_URL = "/og.png";
 export const OG_ALT = `${NAME} - ${DESCRIPTION}`;
 export const OG_TYPE = "image/png";
-export const OG_WIDTH = "1200";
-export const OG_HEIGHT = "630";
+export const OG_WIDTH = 1200;
+export const OG_HEIGHT = 630;
